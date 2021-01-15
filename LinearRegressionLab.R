feda39a0bd17520368feb1c2fcee15c872b6342e
ayee6 <ayee6@jh.edu>
2021-01-15 01:37:39 -0500

    classification and linear regression labs

--- LinearRegressionLab.R
+++ LinearRegressionLab.R
@@ -43,3 +43,22 @@ summary(lm.fit)
 #Multiple linear regression using all 13 predictors in the Boston data set
 lm.fit=lm(medv~., data=Boston)
 summary(lm.fit)
+
+lm.fit1=update(lm.fit, ~.-age)
+summary(lm.fit1)
+
+#Interaction Terms between lstat and age predictors
+summary(lm(medv~lstat*age, data=Boston))
+
+#Non-linear transformation of predictors
+lm.fit2 = lm(medv~lstat+I(lstat^2))
+summary(lm.fit2)
+
+#Qualitative predictors
+names(Carseats)
+lm.fit = lm(Sales~. +Income:Advertising+Price:Age, data=Carseats)
+summary(lm.fit)
+
+attach(Carseats)
+contrasts(ShelveLoc)
+?contrasts
