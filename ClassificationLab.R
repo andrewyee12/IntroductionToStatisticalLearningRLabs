8bb8a102c54d94b712d82c8bf3cf0e633c2ccb15
ayee6 <ayee6@jh.edu>
2021-01-17 11:40:31 -0500

    classification lab

--- ClassificationLab.R
+++ ClassificationLab.R
@@ -40,3 +40,30 @@ glm.pred[glm.probs>.5] = "Up"
 
 #Create confusion matrix to observe correct and incorrect classifications
 table(glm.pred, Direction)
+mean(glm.pred==Direction)
+#Training error rate is 47.8%, however training and testing model on same 
+#training dataset can often lead to overoptimistic training error rate.
+
+#Split data set into training and test data training observations
+#are from 2001 to 2004 and test data will be observations from 2005 onwards
+train=(Year<2005)
+Smarket.2005 = Smarket[!train,]
+dim(Smarket.2005)
+Direction.2005=Direction[!train]
+
+#Predict on new test dataset as 2005
+glm.fits=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume, data=Smarket,
+             family=binomial, subset=train)
+glm.probs=predict(glm.fits,Smarket.2005,type="response")
+glm.pred=rep("Down",252)
+glm.pred[glm.probs > .5]="Up"
+table(glm.pred,Direction.2005)
+mean(glm.pred==Direction.2005)
+mean(glm.pred!=Direction.2005)
+#New error rate using test data set is worst, indicating that it is 
+#difficult to predict future market performance using previous returns
+
+#Linear Discriminant Analysis 
+lda.fit=lda(Direction~Lag1+Lag2,data=Smarket,subset=train)
+lda.fit
+plot(lda.fit)
