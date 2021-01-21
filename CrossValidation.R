a13ede807f0c38d2be8f86ef108f68f1efd064ba
Andrew Yee <ayeepts@gmail.com>
2021-01-21 15:35:25 -0500

    Cross validation

--- CrossValidation.R
+++ CrossValidation.R
@@ -1,6 +1,63 @@
 library(ISLR)
 set.seed(1)
+
+#Validation Set Approach
 train=sample(392,196)
 lm.fit=lm(mpg~horsepower,data=Auto,subset=train)
 attach(Auto)
+plot(mpg,horsepower)
+
+#Predict MSE of the validation set on Linear regression
 mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+#Predict MSE of the validation set on quadratic and cubic regression
+lm.fit2=lm(mpg~poly(horsepower,2),data=Auto,subset=train)
+mean((mpg-predict(lm.fit2,Auto))[-train]^2)
+lm.fit3=lm(mpg~poly(horsepower,3),data=Auto,subset=train)
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+#Sample different validation set to obtain different MSE
+set.seed(2)
+train=sample(392,196)
+lm.fit=lm(mpg~horsepower,subset=train)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+lm.fit2=lm(mpg~poly(horsepower,2),data=Auto,subset=train)
+mean((mpg-predict(lm.fit2,Auto))[-train]^2)
+lm.fit3=lm(mpg~poly(horsepower,3),data=Auto,subset=train)
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+#Leave-One-Out Cross-Validation
+glm.fit=glm(mpg~horsepower,data=Auto)
+coef(glm.fit)
+lm.fit=lm(mpg~horsepower,data=Auto)
+coef(lm.fit)
+
+#LOOCV for linear regression
+library(boot)
+glm.fit=glm(mpg~horsepower,data=Auto)
+cv.err=cv.glm(Auto,glm.fit)
+cv.err$delta
+
+#Polynomial regression
+cv.error=rep(0,5)
+for(i in 1:5){
+  glm.fit=glm(mpg~poly(horsepower,i),data=Auto)
+  cv.error[i]=cv.glm(Auto,glm.fit)$delta[1]
+}
+cv.error
+#See a sharp drop in cross validation error from linear to 
+#quadratic fits 
+
+#K-Fold Cross Validation
+set.seed(17)
+cv.error.10=rep(0,10)
+for(i in 1:10){
+  glm.fit=glm(mpg~poly(horsepower,i),data=Auto)
+  cv.error.10[i]=cv.glm(Auto,glm.fit,K=10)$delta[1]
+}
+cv.error.10
+#Computational time for K-Fold Cross Validation is much faster than
+#Leave One out cross validation. In addition, there is little 
+#evidence that using higher order polynomial terms will result in 
+#lower test error than a quadratic fit. 
+
